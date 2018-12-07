935f356b27c5b5d1f41900f5333fd80a03fb2bee
bb <biabeniamin@gmail.com>
2018-12-07 13:28:31 +0200

    microblaze working

--- FirstOnNexysVideo/FirstOnNexysVideo.runs/synth_1/rundef.js
+++ FirstOnNexysVideo/FirstOnNexysVideo.runs/synth_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 ISEStep( "vivado",
-         "-log sum.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source sum.tcl" );
+         "-log design_1_wrapper.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source design_1_wrapper.tcl" );
 
 
 
---
+++ Microblaze/Microblaze.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_test.elf_on_local.tcl
@@ -0,0 +1,7 @@
+connect -url tcp:127.0.0.1:3121
+configparams mdm-detect-bscan-mask 2
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys Video 210276689267B"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys Video 210276689267B"} -index 0
+dow D:/Beni/Digilent19/Microblaze/Microblaze.sdk/test/Debug/test.elf
+bpadd -addr &main
