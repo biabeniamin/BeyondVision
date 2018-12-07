e186fefb259435233a819f21f46ddd1da08d89a7
bb <biabeniamin@gmail.com>
2018-12-07 21:15:16 +0200

    dump idea to connect hdmi directly

---
+++ project_4/project_4.srcs/sources_1/bd/design_1/synth/design_1.vhd
@@ -0,0 +1,191 @@
+--Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
+--Date        : Fri Dec  7 21:14:42 2018
+--Host        : DESKTOP-871TSOM running 64-bit major release  (build 9200)
+--Command     : generate_target design_1.bd
+--Design      : design_1
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1 is
+  port (
+    TMDS_OUT_clk_n : out STD_LOGIC;
+    TMDS_OUT_clk_p : out STD_LOGIC;
+    TMDS_OUT_data_n : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    TMDS_OUT_data_p : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    hdmi_in_clk_n : in STD_LOGIC;
+    hdmi_in_clk_p : in STD_LOGIC;
+    hdmi_in_data_n : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    hdmi_in_data_p : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    hdmi_in_ddc_scl_i : in STD_LOGIC;
+    hdmi_in_ddc_scl_o : out STD_LOGIC;
+    hdmi_in_ddc_scl_t : out STD_LOGIC;
+    hdmi_in_ddc_sda_i : in STD_LOGIC;
+    hdmi_in_ddc_sda_o : out STD_LOGIC;
+    hdmi_in_ddc_sda_t : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    reset_0 : in STD_LOGIC;
+    sys_clock : in STD_LOGIC
+  );
+  attribute CORE_GENERATION_INFO : string;
+  attribute CORE_GENERATION_INFO of design_1 : entity is "design_1,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=design_1,x_ipVersion=1.00.a,x_ipLanguage=VHDL,numBlks=3,numReposBlks=3,numNonXlnxBlks=2,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,da_board_cnt=3,da_clkrst_cnt=1,synth_mode=OOC_per_IP}";
+  attribute HW_HANDOFF : string;
+  attribute HW_HANDOFF of design_1 : entity is "design_1.hwdef";
+end design_1;
+
+architecture STRUCTURE of design_1 is
+  component design_1_dvi2rgb_0_0 is
+  port (
+    TMDS_Clk_p : in STD_LOGIC;
+    TMDS_Clk_n : in STD_LOGIC;
+    TMDS_Data_p : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    TMDS_Data_n : in STD_LOGIC_VECTOR ( 2 downto 0 );
+    RefClk : in STD_LOGIC;
+    aRst : in STD_LOGIC;
+    vid_pData : out STD_LOGIC_VECTOR ( 23 downto 0 );
+    vid_pVDE : out STD_LOGIC;
+    vid_pHSync : out STD_LOGIC;
+    vid_pVSync : out STD_LOGIC;
+    PixelClk : out STD_LOGIC;
+    aPixelClkLckd : out STD_LOGIC;
+    DDC_SDA_I : in STD_LOGIC;
+    DDC_SDA_O : out STD_LOGIC;
+    DDC_SDA_T : out STD_LOGIC;
+    DDC_SCL_I : in STD_LOGIC;
+    DDC_SCL_O : out STD_LOGIC;
+    DDC_SCL_T : out STD_LOGIC;
+    pRst : in STD_LOGIC
+  );
+  end component design_1_dvi2rgb_0_0;
+  component design_1_clk_wiz_0 is
+  port (
+    resetn : in STD_LOGIC;
+    clk_in1 : in STD_LOGIC;
+    clk_out1 : out STD_LOGIC;
+    locked : out STD_LOGIC
+  );
+  end component design_1_clk_wiz_0;
+  component design_1_rgb2dvi_0_0 is
+  port (
+    TMDS_Clk_p : out STD_LOGIC;
+    TMDS_Clk_n : out STD_LOGIC;
+    TMDS_Data_p : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    TMDS_Data_n : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    aRst : in STD_LOGIC;
+    vid_pData : in STD_LOGIC_VECTOR ( 23 downto 0 );
+    vid_pVDE : in STD_LOGIC;
+    vid_pHSync : in STD_LOGIC;
+    vid_pVSync : in STD_LOGIC;
+    PixelClk : in STD_LOGIC
+  );
+  end component design_1_rgb2dvi_0_0;
+  signal clk_wiz_clk_out1 : STD_LOGIC;
+  signal dvi2rgb_0_DDC_SCL_I : STD_LOGIC;
+  signal dvi2rgb_0_DDC_SCL_O : STD_LOGIC;
+  signal dvi2rgb_0_DDC_SCL_T : STD_LOGIC;
+  signal dvi2rgb_0_DDC_SDA_I : STD_LOGIC;
+  signal dvi2rgb_0_DDC_SDA_O : STD_LOGIC;
+  signal dvi2rgb_0_DDC_SDA_T : STD_LOGIC;
+  signal dvi2rgb_0_PixelClk : STD_LOGIC;
+  signal dvi2rgb_0_RGB_ACTIVE_VIDEO : STD_LOGIC;
+  signal dvi2rgb_0_RGB_DATA : STD_LOGIC_VECTOR ( 23 downto 0 );
+  signal dvi2rgb_0_RGB_HSYNC : STD_LOGIC;
+  signal dvi2rgb_0_RGB_VSYNC : STD_LOGIC;
+  signal hdmi_in_1_CLK_N : STD_LOGIC;
+  signal hdmi_in_1_CLK_P : STD_LOGIC;
+  signal hdmi_in_1_DATA_N : STD_LOGIC_VECTOR ( 2 downto 0 );
+  signal hdmi_in_1_DATA_P : STD_LOGIC_VECTOR ( 2 downto 0 );
+  signal reset_0_1 : STD_LOGIC;
+  signal rgb2dvi_0_TMDS_CLK_N : STD_LOGIC;
+  signal rgb2dvi_0_TMDS_CLK_P : STD_LOGIC;
+  signal rgb2dvi_0_TMDS_DATA_N : STD_LOGIC_VECTOR ( 2 downto 0 );
+  signal rgb2dvi_0_TMDS_DATA_P : STD_LOGIC_VECTOR ( 2 downto 0 );
+  signal sys_clock_1 : STD_LOGIC;
+  signal NLW_clk_wiz_locked_UNCONNECTED : STD_LOGIC;
+  signal NLW_dvi2rgb_0_aPixelClkLckd_UNCONNECTED : STD_LOGIC;
+  attribute X_INTERFACE_INFO : string;
+  attribute X_INTERFACE_INFO of TMDS_OUT_clk_n : signal is "digilentinc.com:interface:tmds:1.0 TMDS_OUT ";
+  attribute X_INTERFACE_INFO of TMDS_OUT_clk_p : signal is "digilentinc.com:interface:tmds:1.0 TMDS_OUT ";
+  attribute X_INTERFACE_INFO of hdmi_in_clk_n : signal is "digilentinc.com:interface:tmds:1.0 hdmi_in CLK_N";
+  attribute X_INTERFACE_INFO of hdmi_in_clk_p : signal is "digilentinc.com:interface:tmds:1.0 hdmi_in CLK_P";
+  attribute X_INTERFACE_INFO of hdmi_in_ddc_scl_i : signal is "xilinx.com:interface:iic:1.0 hdmi_in_ddc SCL_I";
+  attribute X_INTERFACE_INFO of hdmi_in_ddc_scl_o : signal is "xilinx.com:interface:iic:1.0 hdmi_in_ddc SCL_O";
+  attribute X_INTERFACE_INFO of hdmi_in_ddc_scl_t : signal is "xilinx.com:interface:iic:1.0 hdmi_in_ddc SCL_T";
+  attribute X_INTERFACE_INFO of hdmi_in_ddc_sda_i : signal is "xilinx.com:interface:iic:1.0 hdmi_in_ddc SDA_I";
+  attribute X_INTERFACE_INFO of hdmi_in_ddc_sda_o : signal is "xilinx.com:interface:iic:1.0 hdmi_in_ddc SDA_O";
+  attribute X_INTERFACE_INFO of hdmi_in_ddc_sda_t : signal is "xilinx.com:interface:iic:1.0 hdmi_in_ddc SDA_T";
+  attribute X_INTERFACE_INFO of reset : signal is "xilinx.com:signal:reset:1.0 RST.RESET RST";
+  attribute X_INTERFACE_PARAMETER : string;
+  attribute X_INTERFACE_PARAMETER of reset : signal is "XIL_INTERFACENAME RST.RESET, POLARITY ACTIVE_LOW";
+  attribute X_INTERFACE_INFO of reset_0 : signal is "xilinx.com:signal:reset:1.0 RST.RESET_0 RST";
+  attribute X_INTERFACE_PARAMETER of reset_0 : signal is "XIL_INTERFACENAME RST.RESET_0, POLARITY ACTIVE_LOW";
+  attribute X_INTERFACE_INFO of sys_clock : signal is "xilinx.com:signal:clock:1.0 CLK.SYS_CLOCK CLK";
+  attribute X_INTERFACE_PARAMETER of sys_clock : signal is "XIL_INTERFACENAME CLK.SYS_CLOCK, CLK_DOMAIN design_1_sys_clock, FREQ_HZ 100000000, PHASE 0.000";
+  attribute X_INTERFACE_INFO of TMDS_OUT_data_n : signal is "digilentinc.com:interface:tmds:1.0 TMDS_OUT ";
+  attribute X_INTERFACE_INFO of TMDS_OUT_data_p : signal is "digilentinc.com:interface:tmds:1.0 TMDS_OUT ";
+  attribute X_INTERFACE_INFO of hdmi_in_data_n : signal is "digilentinc.com:interface:tmds:1.0 hdmi_in DATA_N";
+  attribute X_INTERFACE_INFO of hdmi_in_data_p : signal is "digilentinc.com:interface:tmds:1.0 hdmi_in DATA_P";
+begin
+  TMDS_OUT_clk_n <= rgb2dvi_0_TMDS_CLK_N;
+  TMDS_OUT_clk_p <= rgb2dvi_0_TMDS_CLK_P;
+  TMDS_OUT_data_n(2 downto 0) <= rgb2dvi_0_TMDS_DATA_N(2 downto 0);
+  TMDS_OUT_data_p(2 downto 0) <= rgb2dvi_0_TMDS_DATA_P(2 downto 0);
+  dvi2rgb_0_DDC_SCL_I <= hdmi_in_ddc_scl_i;
+  dvi2rgb_0_DDC_SDA_I <= hdmi_in_ddc_sda_i;
+  hdmi_in_1_CLK_N <= hdmi_in_clk_n;
+  hdmi_in_1_CLK_P <= hdmi_in_clk_p;
+  hdmi_in_1_DATA_N(2 downto 0) <= hdmi_in_data_n(2 downto 0);
+  hdmi_in_1_DATA_P(2 downto 0) <= hdmi_in_data_p(2 downto 0);
+  hdmi_in_ddc_scl_o <= dvi2rgb_0_DDC_SCL_O;
+  hdmi_in_ddc_scl_t <= dvi2rgb_0_DDC_SCL_T;
+  hdmi_in_ddc_sda_o <= dvi2rgb_0_DDC_SDA_O;
+  hdmi_in_ddc_sda_t <= dvi2rgb_0_DDC_SDA_T;
+  reset_0_1 <= reset_0;
+  sys_clock_1 <= sys_clock;
+clk_wiz: component design_1_clk_wiz_0
+     port map (
+      clk_in1 => sys_clock_1,
+      clk_out1 => clk_wiz_clk_out1,
+      locked => NLW_clk_wiz_locked_UNCONNECTED,
+      resetn => reset_0_1
+    );
+dvi2rgb_0: component design_1_dvi2rgb_0_0
+     port map (
+      DDC_SCL_I => dvi2rgb_0_DDC_SCL_I,
+      DDC_SCL_O => dvi2rgb_0_DDC_SCL_O,
+      DDC_SCL_T => dvi2rgb_0_DDC_SCL_T,
+      DDC_SDA_I => dvi2rgb_0_DDC_SDA_I,
+      DDC_SDA_O => dvi2rgb_0_DDC_SDA_O,
+      DDC_SDA_T => dvi2rgb_0_DDC_SDA_T,
+      PixelClk => dvi2rgb_0_PixelClk,
+      RefClk => clk_wiz_clk_out1,
+      TMDS_Clk_n => hdmi_in_1_CLK_N,
+      TMDS_Clk_p => hdmi_in_1_CLK_P,
+      TMDS_Data_n(2 downto 0) => hdmi_in_1_DATA_N(2 downto 0),
+      TMDS_Data_p(2 downto 0) => hdmi_in_1_DATA_P(2 downto 0),
+      aPixelClkLckd => NLW_dvi2rgb_0_aPixelClkLckd_UNCONNECTED,
+      aRst => '0',
+      pRst => '0',
+      vid_pData(23 downto 0) => dvi2rgb_0_RGB_DATA(23 downto 0),
+      vid_pHSync => dvi2rgb_0_RGB_HSYNC,
+      vid_pVDE => dvi2rgb_0_RGB_ACTIVE_VIDEO,
+      vid_pVSync => dvi2rgb_0_RGB_VSYNC
+    );
+rgb2dvi_0: component design_1_rgb2dvi_0_0
+     port map (
+      PixelClk => dvi2rgb_0_PixelClk,
+      TMDS_Clk_n => rgb2dvi_0_TMDS_CLK_N,
+      TMDS_Clk_p => rgb2dvi_0_TMDS_CLK_P,
+      TMDS_Data_n(2 downto 0) => rgb2dvi_0_TMDS_DATA_N(2 downto 0),
+      TMDS_Data_p(2 downto 0) => rgb2dvi_0_TMDS_DATA_P(2 downto 0),
+      aRst => '0',
+      vid_pData(23 downto 0) => dvi2rgb_0_RGB_DATA(23 downto 0),
+      vid_pHSync => dvi2rgb_0_RGB_HSYNC,
+      vid_pVDE => dvi2rgb_0_RGB_ACTIVE_VIDEO,
+      vid_pVSync => dvi2rgb_0_RGB_VSYNC
+    );
+end STRUCTURE;
