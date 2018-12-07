8bcd64b788bc39341763d3ade07e9fa2ded9e4f6
bb <biabeniamin@gmail.com>
2018-12-07 13:30:07 +0200

    control gpio from microblaze

--- Microblaze/Microblaze.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_test.elf_on_local.tcl
+++ Microblaze/Microblaze.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_test.elf_on_local.tcl
@@ -4,4 +4,5 @@ targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag
 rst -processor
 targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys Video 210276689267B"} -index 0
 dow D:/Beni/Digilent19/Microblaze/Microblaze.sdk/test/Debug/test.elf
-bpadd -addr &main
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Nexys Video 210276689267B"} -index 0
+con
