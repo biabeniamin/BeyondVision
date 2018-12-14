5fc190d0983361758d52ea828bcef527c11a978f
bb <biabeniamin@gmail.com>
2018-12-14 15:57:09 +0200

    new project created

--- Nexys-Video-HDMI/src/bd/hdmi/sim/hdmi.vhd
+++ Nexys-Video-HDMI/src/bd/hdmi/sim/hdmi.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
---Date        : Fri Dec 14 14:03:37 2018
+--Date        : Fri Dec 14 15:32:48 2018
 --Host        : DESKTOP-871TSOM running 64-bit major release  (build 9200)
 --Command     : generate_target hdmi.bd
 --Design      : hdmi
