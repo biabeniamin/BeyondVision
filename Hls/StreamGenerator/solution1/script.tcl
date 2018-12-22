d0c5d3aadd6c84f5beb1ee0e9afe8ffb7e9bac14
bb <biabeniamin@gmail.com>
2018-12-22 16:03:20 +0200

    stream generator implemented

--- Hls/StreamGenerator/solution1/script.tcl
+++ Hls/StreamGenerator/solution1/script.tcl
@@ -3,14 +3,14 @@
 ## Please DO NOT edit it.
 ## Copyright (C) 1986-2018 Xilinx, Inc. All Rights Reserved.
 ############################################################
-open_project MotionDetector2
-set_top Adder2
-add_files MotionDetector2/main.cpp
-add_files -tb MotionDetector2/tester_core.cpp -cflags "-Wno-unknown-pragmas"
+open_project StreamGenerator
+set_top StreamGenerator
+add_files StreamGenerator/a.cpp
+add_files -tb StreamGenerator/test.cpp
 open_solution "solution1"
 set_part {xc7z020clg400-1} -tool vivado
 create_clock -period 10 -name default
-#source "./MotionDetector2/solution1/directives.tcl"
+#source "./StreamGenerator/solution1/directives.tcl"
 csim_design
 csynth_design
 cosim_design
