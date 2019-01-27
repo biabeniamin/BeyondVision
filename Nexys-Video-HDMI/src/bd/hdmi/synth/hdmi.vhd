2f4aab074ff2571e669bf03e9c2476f71fe771e5
Beniamin Bia <biabeniamin@gmail.com>
2019-01-27 12:35:06 +0200

    vivado:video: project updated to newer version of sobel ip

--- Nexys-Video-HDMI/src/bd/hdmi/synth/hdmi.vhd
+++ Nexys-Video-HDMI/src/bd/hdmi/synth/hdmi.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
---Date        : Sun Jan 27 11:36:45 2019
+--Date        : Sun Jan 27 12:12:56 2019
 --Host        : DESKTOP-871TSOM running 64-bit major release  (build 9200)
 --Command     : generate_target hdmi.bd
 --Design      : hdmi
