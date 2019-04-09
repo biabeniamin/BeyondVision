1c8fd34f480732a40e2666c96449633e11735a42
Beniamin Bia <biabeniamin@outlook.com>
2019-04-09 17:22:09 +0300

    linux: downscale works

--- Steganography.cpp
+++ Steganography.cpp
@@ -12,6 +12,12 @@ cv::Mat Steganography::Embed(cv::Mat input, char *data, int size)
 	int data2[500];
 	int length = 0;
 
+	while (input.rows > 768 || input.cols > 1440)
+	{
+		cv::Mat old = input.clone();
+		cv::resize(old, input, cv::Size(), 0.75, 0.75);	
+	}
+
 	Rsa::GetInstance()->Encrypt(data, data2, size);
 
 	return embedder.EmbedData(input, (uchar*)data2, size * 4, &length);
