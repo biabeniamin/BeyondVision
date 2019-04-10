074e12c3bb9e39e403a97d373a197f2e9c072a00
Beniamin Bia <biabeniamin@outlook.com>
2019-04-10 19:26:50 +0300

    linux: copy certificate file for encryption

--- Website/index.php
+++ Website/index.php
@@ -89,6 +89,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		//echo "tesdt2";
 		//echo $_FILES["photo"]["tmp_name"];
 		move_uploaded_file( $_FILES['photo']['tmp_name'], "/var/www/html/motion/image.png" );
+		if(isset($_FILES["certificate"]))
+		{
+			move_uploaded_file( $_FILES['certificate']['tmp_name'], "/var/www/html/motion/public.rsa" );
+		}
 		writeFile("motion/message", $_POST['message']);
 		writeFile("motion/job", "2");
 
@@ -118,6 +122,8 @@ readFile("motion/out.png");*/
 <form action="index.php" method="post" accept-charset="utf-8" enctype="multipart/form-data">
 Encode
     <input type="file" name="photo" id="photo" />
+    Certificate:
+<input type="file" name="certificate" />
 <input type="text" name="message"/>
     <input type="submit" value="Upload" />
 </form>		
