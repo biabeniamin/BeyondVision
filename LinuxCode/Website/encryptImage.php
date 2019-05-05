5f575735ed190af62f556dd8b0b70d1d51c3412e
Beniamin Bia <biabeniamin@outlook.com>
2019-05-05 11:25:35 +0300

    linux: encrypt image stylized

--- LinuxCode/Website/encryptImage.php
+++ LinuxCode/Website/encryptImage.php
@@ -14,17 +14,33 @@
 		writeFile("motion/message", $_POST['message']);
 		writeFile("motion/job", "2");
 		$output = "<a href='motion/out.png'>Output image</a>";
+		$image="<img src='motion/out.png' style='width:500px'/>";
 	}
 
 ?>
+<head>
+	<script>
+		function loading()
+		{
+			document.getElementById("load").style.display="block";
+		}
+	</script>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<style>
+	body {
+		
+	}
+	</style>
+</head>
 <form action="encryptImage.php" method="post" accept-charset="utf-8" enctype="multipart/form-data">
-Encode
-    <input type="file" name="photo" id="photo" />
-    Certificate:
-<input type="file" name="certificate" />
-<input type="text" name="message"/>
+Encode<br>
+    <input type="file" name="photo" id="photo" /><br>
+    Certificate:<br>
+<input type="file" name="certificate" /><br>
+<input type="text" name="message"/><br>
     <input type="submit" value="Upload" />
 </form>		
 <?php
 	echo $output."<br>";
+	echo $image;
 ?>
