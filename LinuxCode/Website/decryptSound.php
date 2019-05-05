d5414340c8e454e4ed775edd4362d0785b7fe629
Beniamin Bia <biabeniamin@outlook.com>
2019-05-05 11:30:24 +0300

    linux: remove unnecessary code

--- LinuxCode/Website/decryptSound.php
+++ LinuxCode/Website/decryptSound.php
@@ -3,25 +3,8 @@
 <?php
 	require 'helpers.php';
 	$output="";
-	if(isset($_FILES["photoDecrypt"]))
-	{
-		echo "test";
-		echo $_FILES["photoDecrypt"]["tmp_name"];
-		move_uploaded_file( $_FILES['photoDecrypt']['tmp_name'], "/var/www/html/motion/image.png" );
-		if(isset($_FILES["certificate"]))
-		{
-			move_uploaded_file( $_FILES['certificate']['tmp_name'], "/var/www/html/motion/private.rsa" );
-		}
-		writeFile("motion/job", "1");
-
-		echo readAllFile("motion/done");
-		while(readAllFile("motion/done")!="1");
-		echo "not ready";
-		$output = readAllFile("motion/message");
-	}
 	if(isset($_FILES["audioDecrypt"]))
 	{
-		echo "test";
 		move_uploaded_file( $_FILES['audioDecrypt']['tmp_name'], "/var/www/html/motion/audioIn.wav" );
 		if(isset($_FILES["certificate"]))
 		{
@@ -29,7 +12,6 @@
 		}
 		writeFile("motion/job", "5");
 
-		echo readAllFile("motion/done");
 		while(readAllFile("motion/done")!="1");
 		$output = readAllFile("motion/message");
 	}
@@ -59,7 +41,6 @@
 	    <input onclick="loading()" type="submit" value="Upload" />
 	</form>		
 	<img id="load" src="loading.gif" style="display:none;width:100px;">
-	<button onclick="loading()">asdf</button>
 	<p style="margin:2%; border:1px dashed black;">
 	<?php
 		echo $output."<br>";
